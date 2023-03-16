a0a433fbdd5925271485e28a9456201778954d8d
Marcus Davanço <md.daves@gmail.com>
2023-03-16 16:30:38 -0300

    feat: tasklist update

--- src/Components/TaskList/Header.tsx
+++ src/Components/TaskList/Header.tsx
@@ -1,16 +1,29 @@
-export function Header() {
+import { TaskType } from "./Task";
+
+interface TaskHeaderProps {
+  tasks: TaskType[];
+}
+
+export function Header({ tasks }: TaskHeaderProps) {
+  const finishedTasks = tasks.reduce((acc, task) => {
+    if (task.isChecked) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
+
   return (
     <header className="flex w-[736px] justify-between mb-6 box h-[19px]">
       <div className="flex gap-2">
         <span className="text-blue-500 font-bold text-sm">Tarefas criadas</span>
         <div className="flex font-gray-200 bg-gray-400 px-2 rounded-full text-[14px] items-center">
-          0
+          {tasks.length}
         </div>
       </div>
       <div className="flex gap-2">
         <span className="text-blue-500 font-bold text-sm">Concluídas</span>
         <div className="flex font-gray-200 bg-gray-400 px-2 rounded-full text-[14px] items-center">
-          0
+          {finishedTasks}
         </div>
       </div>
     </header>
--- src/Components/TaskList/index.tsx
+++ src/Components/TaskList/index.tsx
@@ -1,19 +1,55 @@
+import { useState } from "react";
 import { EmptyList } from "./EmptyList";
 import { Header } from "./Header";
-import { Task } from "./Task";
+import { Task, TaskType } from "./Task";
 
 export function TaskList() {
-  const tasks = [1, 2, 3, 4, 5];
+  const [tasks, setTasks] = useState<TaskType[]>([
+    { id: "1", text: "Walk the dog", isChecked: false },
+    { id: "2", text: "Buy groceries", isChecked: true },
+    { id: "3", text: "Do laundry", isChecked: false },
+    { id: "4", text: "Clean the house", isChecked: true },
+    { id: "5", text: "Pay bills", isChecked: false },
+  ]);
+
+  function deleteTask(taskToDelete: string) {
+    setTasks(tasks.filter((task) => task.id !== taskToDelete));
+  }
+
+  function updateTaskStatus(taskToUpdate: string) {
+    const taskIndex = tasks.findIndex((task) => task.id === taskToUpdate);
+
+    const updatedTask = {
+      ...tasks[taskIndex],
+      isChecked: !tasks[taskIndex].isChecked,
+    };
+
+    const updatedTasks = [...tasks];
+    updatedTasks[taskIndex] = updatedTask;
+
+    setTasks(updatedTasks);
+  }
 
   return (
     <div className="gap-6">
-      <Header />
-      {/* <EmptyList /> */}
-      <div className="flex flex-col gap-3">
-        {tasks.map((task) => {
-          return <Task />;
-        })}
-      </div>
+      <Header tasks={tasks} />
+
+      {tasks.length === 0 ? (
+        <EmptyList />
+      ) : (
+        <div className="flex flex-col gap-3">
+          {tasks.map((task) => {
+            return (
+              <Task
+                key={task.id}
+                task={task}
+                onDeleteTask={deleteTask}
+                onUpdateTask={updateTaskStatus}
+              />
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
